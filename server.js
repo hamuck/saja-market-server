1025dd8f8f46672dfacb1dbd12ed7ff8152e02ce
unknown <qkrdpwls2002@naver.com>
2023-10-09 19:43:53 +0900

    사진 업로드

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
-//express.js
 const express = require("express");
-const app = express();
 const cors = require("cors");
-const port = 8080;
+const app = express();
 const models = require("./models");
-app.use(express.json()); //클라이언트가 json 형식으로 데이터를 보냈을 때 처리할 수 있게 설정하는 코드
-app.use(cors()); //브라우저의 CORS 이슈를 막기 위해 사용하는 코드
+const multer = require("multer");
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "uploads/");
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    },
+  }),
+});
+const port = 8080;
 
-app.get("/products", async (req, res) => {
+app.use(express.json());
+app.use(cors());
+app.use("/uploads", express.static("uploads"));
+
+app.get("/products", (req, res) => {
   models.Product.findAll({
     order: [["createdAt", "DESC"]],
-    attributes: ["id", "name", "price", "createdAt", "seller", 'imageUrl'],
+    attributes: ["id", "name", "price", "createdAt", "seller", "imageUrl"],
   })
     .then((result) => {
       console.log("PRODUCTS : ", result);
@@ -20,31 +32,32 @@ app.get("/products", async (req, res) => {
     })
     .catch((error) => {
       console.error(error);
-      res.send("에러 발생");
+      res.status(400).send("에러 발생");
     });
 });
 
-app.post("/products", async (req, res) => {
+app.post("/products", (req, res) => {
   const body = req.body;
-  const { name, description, price, seller } = body;
-  if (!name || !decodeURI || !price || !seller) {
-    return res.send("모든 필드를 입력해주세요");
+  const { name, description, price, seller, imageUrl } = body;
+  if (!name || !description || !price || !seller || !imageUrl) {
+    res.status(400).send("모든 필드를 입력해주세요");
   }
   models.Product.create({
     name,
     description,
     price,
     seller,
+    imageUrl,
   })
     .then((result) => {
-      console.log("상품 생성 결과: ", result);
+      console.log("상품 생성 결과 : ", result);
       res.send({
         result,
       });
     })
     .catch((error) => {
       console.error(error);
-      res.send("상품 업로드에 문제가 발생했습니다");
+      res.status(400).send("상품 업로드에 문제가 발생했습니다");
     });
 });
 
@@ -64,13 +77,20 @@ app.get("/products/:id", (req, res) => {
     })
     .catch((error) => {
       console.error(error);
-      res.send("상품 조회에 에러가 발생했습니다.");
+      res.status(400).send("상품 조회에 에러가 발생했습니다");
     });
 });
 
-//세팅한 app을 실행시킨다.
+app.post("/image", upload.single("image"), (req, res) => {
+  const file = req.file;
+  console.log(file);
+  res.send({
+    imageUrl: file.path,
+  });
+});
+
 app.listen(port, () => {
-  console.log("그랩의 쇼핑몰 서버가 돌아가고 있습니다.");
+  console.log("그랩의 쇼핑몰 서버가 돌아가고 있습니다");
   models.sequelize
     .sync()
     .then(() => {
@@ -78,7 +98,7 @@ app.listen(port, () => {
     })
     .catch((err) => {
       console.error(err);
-      console.log("DB 연결 실패");
+      console.log("DB 연결 에러ㅠ");
       process.exit();
     });
 });
